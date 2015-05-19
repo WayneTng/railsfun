34821c62aac88994bc254901decb9b9463054b10
wayne <wayne@futureworkz.com>
2015-05-19 10:56:42 +0700

    set title to lowercase in product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,8 +1,12 @@
 class Product < ActiveRecord::Base
+  before_validation :strip_html_from_description
+  before_validation :set_title_to_lower_case
+
   validates :title, :description, :price, presence: true
   validates_numericality_of :price, :on => :create, :greater_than => 0
   #validates numericality: {greater_than: 0, allow_blank = false}
   validate :title_is_shorter_than_description
+  
   scope :published,            ->       { where(published: true) }
   scope :priced_more_than,     ->(price){ where('price > ?', price) }
   scope :description_includes, ->(text) { where('description ILIKE ?',"%#{text}%") }
@@ -12,5 +16,13 @@ class Product < ActiveRecord::Base
     if description.length < title.length
       errors.add(:description, 'can\'t be shorter than title')
     end
+  end
+
+  def strip_html_from_description
+    self.description = ActionView::Base.full_sanitizer.sanitize(self.description)
+  end
+
+  def set_title_to_lower_case
+    self.title = self.title.downcase
   end
 end

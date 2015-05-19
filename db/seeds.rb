29dc9e1ba89bcb3f9cc65254f36f94dff7b43937
wayne <wayne@futureworkz.com>
2015-05-19 09:26:57 +0700

    adding seeds data into product table

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Product.destroy_all
+Product.create!(title: 'Ruby Book', description: 'Learn Ruby Programming', price: 12.99)
+Product.create!(title: 'jQuery Book', description: 'Learn jQuery', price: 11.99)
+Product.create!(title: 'SASS Book', description: 'Learn CSS with SASS', price: 19.99)

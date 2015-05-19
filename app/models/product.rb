c57950e1789963c5fc888c1bbf8f103091380530
wayne <wayne@futureworkz.com>
2015-05-19 10:27:49 +0700

    added scoped to model for product table

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,6 +3,9 @@ class Product < ActiveRecord::Base
   validates_numericality_of :price, :on => :create, :greater_than => 0
   #validates numericality: {greater_than: 0, allow_blank = false}
   validate :title_is_shorter_than_description
+  scope :published,            ->       { where(published: true) }
+  scope :priced_more_than,     ->(price){ where('price > ?', price) }
+  scope :description_includes, ->(text) { where('description ILIKE ?',"%#{text}%") }
 
   def title_is_shorter_than_description
     return if title.blank? or description.blank?
